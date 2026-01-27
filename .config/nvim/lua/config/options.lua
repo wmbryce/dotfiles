af7723c72249045a2ec41b52c81c4807cc73300a
Michael Bryce <michaelbryce@me.com>
2026-01-26 22:02:50 -0700

    fix(files)

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,38 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+
+local opt = vim.opt
+
+-- Line numbers
+opt.relativenumber = true -- Relative line numbers
+
+-- Indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.smartindent = true
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- UI
+opt.scrolloff = 8 -- Keep 8 lines above/below cursor
+opt.sidescrolloff = 8
+opt.signcolumn = "yes" -- Always show sign column
+opt.cursorline = true
+
+-- Splits
+opt.splitright = true
+opt.splitbelow = true
+
+-- Performance
+opt.updatetime = 250
+opt.timeoutlen = 300
+
+-- Undo
+opt.undofile = true
+opt.undolevels = 10000
+
+-- Clipboard
+opt.clipboard = "unnamedplus" -- Use system clipboard
--- .config/nvim/lua/plugins/code.lua
+++ .config/nvim/lua/plugins/code.lua
@@ -2,134 +2,78 @@ return {
   {
     "numToStr/Comment.nvim",
     opts = {
-      ---Add a space b/w comment and the line
       padding = true,
-      ---Whether the cursor should stay at its position
       sticky = true,
-      ---Lines to be ignored while (un)comment
       ignore = nil,
-      ---LHS of toggle mappings in NORMAL mode
       toggler = {
-        ---Line-comment toggle keymap
         line = "gcc",
-        ---Block-comment toggle keymap
         block = "gbc",
       },
-      ---LHS of operator-pending mappings in NORMAL and VISUAL mode
       opleader = {
-        ---Line-comment keymap
         line = "gc",
-        ---Block-comment keymap
         block = "gb",
       },
-      ---LHS of extra mappings
       extra = {
-        ---Add comment on the line above
         above = "gcO",
-        ---Add comment on the line below
         below = "gco",
-        ---Add comment at the end of line
         eol = "gcA",
       },
-      ---Enable keybindings
-      ---NOTE: If given `false` then the plugin won't create any mappings
       mappings = {
-        ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
         basic = true,
-        ---Extra mapping; `gco`, `gcO`, `gcA`
         extra = true,
       },
-      ---Function to call before (un)comment
-      -- pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
       pre_hook = nil,
-      ---Function to call after (un)comment
       post_hook = nil,
     },
   },
   {
     "luckasRanarison/tailwind-tools.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
-    opts = {}, -- your configuration
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    sources = {
-      { name = "supermaven" },
-    },
-    keys = {},
+    opts = {},
   },
   {
     "L3MON4D3/LuaSnip",
     keys = {
       { "<Tab>", false, mode = { "i", "s" } },
-      { "<s-Tab>", false, mode = { "i", "s" } },
+      { "<S-Tab>", false, mode = { "i", "s" } },
     },
   },
   {
     "hrsh7th/nvim-cmp",
     keys = {
-      { "<tab>", false, mode = { "i", "s" } },
-      { "<s-tab>", false, mode = { "i", "s" } },
+      { "<Tab>", false, mode = { "i", "s" } },
+      { "<S-Tab>", false, mode = { "i", "s" } },
     },
   },
-  -- {
-  --   "supermaven-inc/supermaven-nvim",
-  --   config = function()
-  --     require("supermaven-nvim").setup({
-  --       keymaps = {
-  --         accept_suggestion = "<Tab>",
-  --         clear_suggestion = "<C-]>",
-  --         accept_word = "<C-j>",
-  --       },
-  --       ignore_filetypes = { cpp = true },
-  --       color = {
-  --         suggestion_color = "#ffffff",
-  --         cterm = 244,
-  --       },
-  --       log_level = "info", -- set to "off" to disable logging completely
-  --       disable_inline_completion = false, -- disables inline completion for use with cmp
-  --       disable_keymaps = false, -- disables built in keymaps for more manual control
-  --     })
-  --   end,
-  -- },
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
     lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    opts = {
-      -- add any opts here
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    version = false,
+    opts = {},
     build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      "nvim-tree/nvim-web-devicons",
+      "zbirenbaum/copilot.lua",
       {
-        -- support for image pasting
         "HakonHarnes/img-clip.nvim",
         event = "VeryLazy",
         opts = {
-          -- recommended settings
           default = {
             embed_image_as_base64 = false,
             prompt_for_file_name = false,
             drag_and_drop = {
               insert_mode = true,
             },
-            -- required for Windows users
             use_absolute_path = true,
           },
         },
       },
       {
-        -- Make sure to set this up properly if you have lazy=true
         "MeanderingProgrammer/render-markdown.nvim",
         opts = {
           file_types = { "markdown", "Avante" },
